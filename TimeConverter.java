adff31b4e90cb74252d7624145462594cead3a5d
Jason Bodzy <jasonbodzy@gmail.com>
2021-09-16 13:36:16 -0400

    Create TimeConverter.java

---
+++ TimeConverter.java
@@ -0,0 +1,38 @@
+public class TimeConverter {
+
+
+    public static String timeConversion(String s) {
+        // Write your code here
+
+        String[] splitTime = s.split(":");
+        int newTime = Integer.parseInt(splitTime[0]);
+
+
+        if(s.contains("P") && newTime != 12) {
+            newTime += 12;
+        }
+
+        if(s.contains("A") && newTime == 12){
+            char[] finalChars = splitTime[2].toCharArray();
+            String output = ("00" + ":" + splitTime[1] + ":" + finalChars[0] + finalChars[1]);
+            return(output);
+        } else if (s.contains("A")){
+            return(s.substring(0, 8));
+        } else if (s.contains("P") && newTime == 12){
+            return(s.substring(0, 8));
+        } else {
+            char[] finalChars = splitTime[2].toCharArray();
+            String output = (newTime + ":" + splitTime[1] + ":" + finalChars[0] + finalChars[1]);
+            return(output);
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(timeConversion("12:12:02AM"));
+    }
+
+
+}
